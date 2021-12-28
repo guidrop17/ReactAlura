d3267ab73c32f8277ca91672430a2b680c2e4780
Guilherme Araujo <guidrop17@gmail.com>
2021-12-28 17:10:40 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <form>
+      <input type="Text" placeholder="Titulo"/>
+      <textarea placeholder="Escreva sua nota"/>
+      <button>Criar Nota</button>
+
+    </form>
+  );
+}
+
+export default App;
